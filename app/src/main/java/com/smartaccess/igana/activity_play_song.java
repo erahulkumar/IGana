295abc68300dd8a2e616abdd8a26738e53348d7f
er-rahul-kumar <rahulkumarchuahantc@gmail.com>
2022-01-10 15:52:24 +0530

    hello

---
+++ app/src/main/java/com/smartaccess/igana/activity_play_song.java
@@ -0,0 +1,14 @@
+package com.smartaccess.igana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_play_song extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_song);
+    }
+}
